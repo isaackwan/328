3c0faed8cdd39df8b09e185a29b315fce09d7be3
Isaac Kwan <g@isaac.pw>
2017-04-11 22:57:05 +0800

    Working...

--- src/sample/VideoStreamer.java
+++ src/sample/VideoStreamer.java
@@ -1,28 +1,41 @@
 package sample;
 
 import fi.iki.elonen.NanoHTTPD;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
-import java.util.Map;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 /**
  * Created by isaac on 4/11/17.
  */
 public class VideoStreamer extends NanoHTTPD {
+    private PipedOutputStream stream = new PipedOutputStream();
+    private PipedInputStream outstream = new PipedInputStream(stream);
+
     public VideoStreamer(RemoteSong song) throws IOException {
         super(9011);
         start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-        Logger.getLogger("VideoStreamer").info("Started web server for VideoStreamer");
+        Logger.getLogger("VideoStreamer").info("Started web server for VideoStreamer at port 9011");
     }
 
     @Override
     public Response serve(IHTTPSession session) {
-        if (session.getMethod() == Method.HEAD) {
-            return newChunkedResponse(Response.Status.OK, "video/mp4", null);
-        }
-        final Map<String,String> headers = session.getHeaders();
-        throw new NotImplementedException();
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                try {
+                    stream.write(99);
+                    stream.write(70);
+                } catch (IOException ex) {
+                    Logger.getLogger("VideoStreamer").warning("Failed to copy to video streamer.");
+                }
+            }
+        }, 0, 3, TimeUnit.SECONDS);
+        return newChunkedResponse(Response.Status.OK, "video/mp4", outstream);
     }
 }
